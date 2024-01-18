62ec5efef164c8f91d9db93f6c8f7898da706514
Jean Bomhof <92791787+Jeanx23@users.noreply.github.com>
2024-01-19 00:45:29 +0100

    Optimization of how the data get requested (Added Filter). Implemented UserControl for MainWindow.

---
+++ praktischeInformatikJB/Converters/CalenderColorConverter.cs
@@ -0,0 +1,31 @@
+﻿using praktischeInformatikJB.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace praktischeInformatikJB.Converters
+{
+    internal class CalenderColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            SelectionState selectionState = (SelectionState)value;
+
+            if (selectionState == SelectionState.Calender)
+            {
+                return System.Windows.Media.Colors.LightGreen;
+            }
+
+            return System.Windows.Media.Colors.Transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ praktischeInformatikJB/Model/DateFilter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using praktischeInformatikJB.Interfaces;
+using praktischeInformatikJB.ViewModels;
+using sportapiwrapper.Enums;
+using sportapiwrapper.InternalLogic;
+using sportapiwrapper.models;
+
+namespace praktischeInformatikJB.Model
+{
+    public class DateFilter : IFilter
+    {
+        public League SelectedLeague { get; }
+
+        public DateOnly FilterDate { get; }
+
+        public DateFilter(League league, DateTime filterDate)
+        {
+            SelectedLeague = league;
+            FilterDate = DateOnly.FromDateTime(filterDate);
+        }
+
+        public List<MatchViewModel> Filter()
+        {
+            if (SelectedLeague.LeagueShortcut == null)
+            {
+                throw new Exception("League needs a shotcut");
+            }
+
+            string year = "2023";
+            string leagueShortcut = SelectedLeague.LeagueShortcut;
+
+            List<MatchData>? matches = SportsApi.GetAllAvailableMatchDayData(leagueShortcut, year, out ReturnStatus status);
+
+            if (matches == null)
+            {
+                throw new Exception("Could not get match data");
+            }
+
+            List<MatchData> matchesToday = matches.Where(x =>
+            {
+                if (x.MatchDateTimeUTC == null)
+                {
+                    return false;
+                }
+
+                DateTime matchDateTime = x.MatchDateTimeUTC.Value.Date;
+                DateOnly matchDate = DateOnly.FromDateTime(matchDateTime);
+                return matchDate == FilterDate;
+            }).ToList();
+
+            List<MatchViewModel> matchViewModels = matchesToday.Select(x => new MatchViewModel(x, leagueShortcut)).ToList();
+            return matchViewModels;
+        }
+    }
+}
